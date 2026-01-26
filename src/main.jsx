a8a76a965a76ebe8688e6a4e55cca9aba7150d13
Edgar-MedCor <edgarmen159@gmail.com>
2026-01-26 17:19:02 -0500

    .

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-// intenta montar en WordPress primero
-const wpContainer = document.getElementById("tracking-app");
-
-// fallback para desarrollo local
-const localContainer = document.getElementById("root");
-
-const container = wpContainer || localContainer;
-
-if (container) {
-  createRoot(container).render(
-    <StrictMode>
-      <App />
-    </StrictMode>
-  );
-}
+createRoot(document.getElementById('tracking-app')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
