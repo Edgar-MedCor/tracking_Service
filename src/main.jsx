4c439faaf3cafe2f5202b8ce6f3610519f24cece
Edgar-MedCor <edgarmen159@gmail.com>
2026-01-26 17:09:05 -0500

    Configuracion para integracion en wp

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// intenta montar en WordPress primero
+const wpContainer = document.getElementById("tracking-app");
+
+// fallback para desarrollo local
+const localContainer = document.getElementById("root");
+
+const container = wpContainer || localContainer;
+
+if (container) {
+  createRoot(container).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+}
